2d4d82830ff41ecfbcbc9922aa8c8913d4a0a565
Parth Degama <parthka.2005@gmail.com>
2022-05-04 11:01:34 +0530

    edit import url

--- test.ts
+++ test.ts
@@ -1,4 +1,4 @@
-import QRCode from './mod.ts';
+import QRCode from 'https://deno.land/x/qrust_bridge@v1.0/mod.ts';
 
 let q1 = new QRCode("I Am QR Code", 128, "FFEF82", "EB5353");
 new QRCode("Hello, World!", 256, "2155CD").save("qr/hello");
